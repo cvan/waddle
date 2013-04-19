3ba27274f578032e52797dafb1c76e8875c2024c
Chris Van <cvan@mozilla.com>
2013-04-18 20:01:10 -0700

    add settings_local.dist.js

---
+++ settings_local.dist.js
@@ -0,0 +1,10 @@
+exports.host = 'localhost';
+exports.port = '7777';
+exports.mongo = {
+    hostname: 'localhost',
+    port: 27017,
+    username: '',
+    password: '',
+    name: '',
+    db: 'waddle'
+};
